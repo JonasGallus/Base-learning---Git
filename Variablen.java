d0a11cdc2491adcf2b1652d24f5e85c914230482
Magier <jonasgallus@gmx.de>
2024-10-04 14:24:14 +0200

    Variablen

---
+++ Variablen.java
@@ -0,0 +1,16 @@
+public class Variablen {
+    int num1 = 4;
+    int num2 = 8;
+    int sum = num1 + num2;
+    double num3 = 3.1415;
+    String text1 = "Das ist ein Text";
+    long num4 = 434345345;
+    boolean wert1 = true;
+    boolean wert2 = false;
+    public void doSomthing() {
+        System.out.println(sum);
+        System.out.println("Die Summe aus " + num1 + " und " + num2 + 
+        " ist gleich " + sum);
+    }
+
+}
